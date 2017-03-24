46f94dc264306ac6d3397d09bb4aa1200d334c2b
Prisoner Sense <EternityKnowsMyName@hotmail.com>
2017-03-24 23:42:47 +0200

    Add basic validation.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using GigHub.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]     
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get { return DateTime.Parse(string.Format($"{Date} {Time}")); }
+            return DateTime.Parse(string.Format($"{Date} {Time}"));
         }
     }
 }
