0feed54f9e2e45142efbbb92e36da14449b3ce6b
Prisoner Sense <prisonersense@yahoo.com>
2017-04-03 20:43:01 +0300

    Add IsCanceled to Gig, add Bootbox and clean up.

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -21,8 +21,10 @@ namespace GigHub.Controllers.Api
             var userId = User.Identity.GetUserId();
             var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
 
-            gig.IsCanceled = true;
+            if(gig.IsCanceled)
+                return NotFound();
 
+            gig.IsCanceled = true;
             _context.SaveChanges();
 
             return Ok();
