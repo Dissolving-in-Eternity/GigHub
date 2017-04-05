bbccf788da6e9d9962e779ad40152e9317ef6c1b
Prisoner Sense <prisonersense@yahoo.com>
2017-04-05 19:08:18 +0300

    Move dtos to separate folder.

--- GigHub/Controllers/Api/NotificationsController.cs
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 using AutoMapper;
+using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
 
@@ -24,7 +25,7 @@ namespace GigHub.Controllers.Api
             var userId = User.Identity.GetUserId();
 
             var notifications = _context.UserNotifications
-                .Where(un => un.UserId == userId)
+                .Where(un => un.UserId == userId && !un.IsRead)
                 .Select(un => un.Notification)
                 .Include(n => n.Gig.Artist)
                 .ToList();
