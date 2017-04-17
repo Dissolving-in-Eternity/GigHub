502fb484935adce033154744c8a7f76210423595
Prisoner Sense <prisonersense@yahoo.com>
2017-04-17 16:20:16 +0300

    Configure Identity and mock current user.

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+
+        public GigsControllerTests()
+        {
+            
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            _controller = new GigsController(mockUnitOfWork.Object);
+            _controller.MockCurrentUser("1", "user@domain.com");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
