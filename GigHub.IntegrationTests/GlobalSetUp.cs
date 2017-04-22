ccb70d96ddc83fa8cc4e372b3412f920e9b2d05d
Prisoner Sense <prisonersense@yahoo.com>
2017-04-22 18:29:13 +0300

    Set up NUint and integration tests project.

---
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace GigHub.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetUp
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Reference to configuration class of code-first migrations
+            // In order to bring our integration test DB to the latest version
+            var configuration = new GigHub.Migrations.Configuration();
+
+            // DB-migrator object
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+    }
+}
---
+++ GigHub.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("862a2ad9-1084-41f2-a577-a940721fe3f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
