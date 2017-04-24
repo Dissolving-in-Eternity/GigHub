f3b6cc8dd5ce74d3495715549aa0c23df9dfef28
Prisoner Sense <prisonersense@yahoo.com>
2017-04-24 14:56:13 +0300

    Add Isolated attribute for DB transactions.

---
+++ GigHub.IntegrationTests/Isolated.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Transactions;
+using NUnit.Framework;
+
+namespace GigHub.IntegrationTests
+{
+    // Any changes we make to the data in the DB will be automatically rolled back
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+
+        public void BeforeTest(TestDetails testDetails)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(TestDetails testDetails)
+        {
+            _transactionScope.Dispose();
+        }
+
+        // This attribute can only be applied to test methods
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+}
